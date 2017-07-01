3afcc28c79448f878214cede89a99306c4606d09
wubaiqing <wubaiqing@vip.qq.com>
2017-07-01 23:50:47 +0800

    优化代码

--- 1.py
+++ 1.py
@@ -4,19 +4,18 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-import os
 import requests
 import json
 from selenium import webdriver
 
 
-r = requests.get('http://we.40zhe.com/api/getAllArticles')
+r = requests.get('http://article.app/api/getAllArticles')
 if (r.status_code != 200):
-    os._exit(0)
+    sys.exit()
 
 obj = json.loads(r.text, encoding='utf-8')
 if (len(obj) == 0):
-    os._exit(0)
+    sys.exit()
 
 driver = webdriver.Chrome()
 for item in obj:
@@ -32,12 +31,12 @@ for item in obj:
         print 'taobaoke : %s' % item['taobaoke_url']
         title = driver.title.replace('-淘宝网', '')
         title = title.replace('-tmall.com天猫', '')
-        url = 'http://we.40zhe.com/api/setArticleAttr?id=%s&taobao_id=%s&title=%s' % (item['id'], itemId, title);
+        url = 'http://article.app/api/setArticleAttr?id=%s&taobao_id=%s&title=%s' % (item['id'], itemId, title)
         print 'crawl : %s' % url.encode('utf-8')
         rs = requests.get(url)
         print rs.status_code
     except Exception as e:
-        url = 'http://we.40zhe.com/api/deleteArticle?id=%s' % item['id'];
+        url = 'http://article.app/api/deleteArticle?id=%s' % item['id'];
         rs = requests.get(url)
         print 'delete %s' % item['id']
         print e
