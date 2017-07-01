2f33fe48c8fca3b53ed414f7c6e9baf780cec432
wubaiqing <wubaiqing@vip.qq.com>
2017-07-01 23:54:28 +0800

    优化代码

--- 1.py
+++ 1.py
@@ -9,7 +9,7 @@ import json
 from selenium import webdriver
 
 
-r = requests.get('http://article.app/api/getAllArticles')
+r = requests.get('http://we.40zhe.com/api/getAllArticles')
 if (r.status_code != 200):
     sys.exit()
 
@@ -31,12 +31,12 @@ for item in obj:
         print 'taobaoke : %s' % item['taobaoke_url']
         title = driver.title.replace('-淘宝网', '')
         title = title.replace('-tmall.com天猫', '')
-        url = 'http://article.app/api/setArticleAttr?id=%s&taobao_id=%s&title=%s' % (item['id'], itemId, title)
+        url = 'http://we.40zhe.com/api/setArticleAttr?id=%s&taobao_id=%s&title=%s' % (item['id'], itemId, title)
         print 'crawl : %s' % url.encode('utf-8')
         rs = requests.get(url)
         print rs.status_code
     except Exception as e:
-        url = 'http://article.app/api/deleteArticle?id=%s' % item['id'];
+        url = 'http://we.40zhe.com/api/deleteArticle?id=%s' % item['id'];
         rs = requests.get(url)
         print 'delete %s' % item['id']
         print e
