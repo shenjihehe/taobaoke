a48fe3253737977d0ba7e3527f7ea76a38944e17
wubaiqing <wubaiqing@vip.qq.com>
2017-07-02 18:43:44 +0800

    淘宝客链接转换

--- 2.py
+++ 2.py
@@ -10,8 +10,8 @@ import json
 from selenium import webdriver
 
 while True:
-    print 'article taobaoke run'
-    r = requests.get('http://we.40zhe.com/api/getAllArticles')
+    print 'invengory taobaoke run'
+    r = requests.get('http://article.app/api/getITBKGoods')
     if (r.status_code != 200):
         time.sleep(5)
         continue
@@ -22,9 +22,9 @@ while True:
         continue
 
     driver = webdriver.Chrome()
-    for item in obj:
-        driver.get(item['taobaoke_url'])
+    for item in obj['data']:
         try:
+            driver.get(item['taobaoke_url'])
             try:
                 ele = driver.find_element_by_name("item_id")
                 itemId = ele.get_attribute("value")
@@ -32,17 +32,13 @@ while True:
                 ele = driver.find_element_by_id("LineZing")
                 itemId = ele.get_attribute("itemid")
 
-            print 'taobaoke : %s' % item['taobaoke_url']
-            title = driver.title.replace('-淘宝网', '')
-            title = title.replace('-tmall.com天猫', '')
-            url = 'http://we.40zhe.com/api/setArticleAttr?id=%s&taobao_id=%s&title=%s' % (item['id'], itemId, title)
-            print 'crawl : %s' % url.encode('utf-8')
+            print 'crawl taobaoId: %s' % itemId
+            url = 'http://we.40zhe.com/api/setITBKGoodsr?inventory_goods_id=%s&item_id=%s' % (item['inventory_goods_id'], itemId)
+            print 'taobaoId crawl : %s' % url.encode('utf-8')
             rs = requests.get(url)
             print rs.status_code
         except Exception as e:
-            url = 'http://we.40zhe.com/api/deleteArticle?id=%s' % item['id'];
-            rs = requests.get(url)
-            print 'delete %s' % item['id']
             print e
+            pass
 
     driver.quit()
