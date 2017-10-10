b3be63f19d4903ba705b0b794c89ac708b4c57e2
wubaiqing <wubaiqing@vip.qq.com>
2017-10-10 17:39:49 +0800

    .

--- article_goods.py
+++ article_goods.py
@@ -25,55 +25,54 @@ while True:
         time.sleep(5)
         continue
 
-    obj = json.loads(r.text, encoding='utf-8')
-    if (len(obj) == 0):
+    item = json.loads(r.text, encoding='utf-8')
+    if (len(item) == 0):
         time.sleep(5)
         continue
 
-    for item in obj:
-        driver = webdriver.Chrome()
+    driver = webdriver.Chrome()
 
+    try:
+        driver.get(item['taobaoke_url'])
+    except:
+        url = 'http://we.40zhe.com/api/deleteArticle?id=%s' % (item['id'])
+        rs = requests.get(url)
+        print 'Request Url Error'
+        print 'Delete taobao id %s' % item['taobao_id']
+        print url
+        driver.quit()
+        continue
+
+    try:
         try:
-            driver.get(item['taobaoke_url'])
+            # 淘宝
+            ele = driver.find_element_by_name("item_id")
+            itemId = ele.get_attribute("value")
+            img = driver.find_element_by_id('J_ImgBooth').get_attribute("src")
+            price = driver.find_element_by_class_name('tb-rmb-num').text
         except:
-            url = 'http://we.40zhe.com/api/deleteArticle?id=%s' % (item['id'])
-            rs = requests.get(url)
-            print 'Request Url Error'
-            print 'Delete taobao id %s' % item['taobao_id']
-            print url
-            driver.quit()
-            continue
+            # 天猫
+            ele = driver.find_element_by_id("LineZing")
+            itemId = ele.get_attribute("itemid")
+            img = driver.find_element_by_id('J_ImgBooth').get_attribute("src")
+            price = driver.find_element_by_class_name('tm-price').text
 
-        try:
-            try:
-                # 淘宝
-                ele = driver.find_element_by_name("item_id")
-                itemId = ele.get_attribute("value")
-                img = driver.find_element_by_id('J_ImgBooth').get_attribute("src")
-                price = driver.find_element_by_class_name('tb-rmb-num').text
-            except:
-                # 天猫
-                ele = driver.find_element_by_id("LineZing")
-                itemId = ele.get_attribute("itemid")
-                img = driver.find_element_by_id('J_ImgBooth').get_attribute("src")
-                price = driver.find_element_by_class_name('tm-price').text
-
-            print 'taobaoke : %s' % item['taobaoke_url']
-            title = driver.title.replace('-淘宝网', '')
-            title = title.replace('-tmall.com天猫', '')
-            url = 'http://we.40zhe.com/api/setArticleAttr?id=%s&taobao_id=%s&title=%s&img_src=%s&price=%s' % (item['id'], itemId, title, img, price)
-            print 'crawl : %s' % url.encode('utf-8')
-            rs = requests.get(url)
-            print rs.status_code
-            driver.quit()
-        except Exception as e:
-            print 'Request Url Error'
-            print 'Delete taobao id %s' % item['taobao_id']
-            print e
-            url = 'http://we.40zhe.com/api/deleteArticle?id=%s' % (item['id'])
-            rs = requests.get(url)
-            print url
-            driver.quit()
+        print 'taobaoke : %s' % item['taobaoke_url']
+        title = driver.title.replace('-淘宝网', '')
+        title = title.replace('-tmall.com天猫', '')
+        url = 'http://we.40zhe.com/api/setArticleAttr?id=%s&taobao_id=%s&title=%s&img_src=%s&price=%s' % (item['id'], itemId, title, img, price)
+        print 'crawl : %s' % url.encode('utf-8')
+        rs = requests.get(url)
+        print rs.status_code
+        driver.quit()
+    except Exception as e:
+        print 'Request Url Error'
+        print 'Delete taobao id %s' % item['taobao_id']
+        print e
+        url = 'http://we.40zhe.com/api/deleteArticle?id=%s' % (item['id'])
+        rs = requests.get(url)
+        print url
+        driver.quit()
 
 
     driver.quit()
