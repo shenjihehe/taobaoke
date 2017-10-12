e387e4fc9e97461da3d17353c27806af0eb6ce9c
wubaiqing <wubaiqing@vip.qq.com>
2017-10-12 11:50:19 +0800

    优化逻辑

--- article_goods.py
+++ article_goods.py
@@ -21,18 +21,20 @@ while True:
         print e
         continue
 
+    print r.text
     if (r.status_code != 200):
         time.sleep(5)
         continue
 
     try:
-        item = json.loads('asdf', encoding='utf-8')
+        item = json.loads(r.text, encoding='utf-8')
         if (len(item) == 0):
             time.sleep(5)
             continue
     except Exception as e:
         time.sleep(2)
         continue
+
 
 
     driver = webdriver.Chrome()
